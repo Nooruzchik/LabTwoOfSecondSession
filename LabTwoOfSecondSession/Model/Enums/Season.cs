80b59fefcf96b2a4b1a9d31976b384b591c1a2a0
Nooruz Suleimanov <suleimanovnooruz@gmail.com>
2026-02-19 00:16:33 +0700

    Добавлены все  перечисления

---
+++ LabTwoOfSecondSession/Model/Enums/Season.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTwoOfSecondSession.Model.Enums
+{
+    public enum Season
+    {
+        winter,
+        spring,
+        summer,
+        autumn
+    }
+}
